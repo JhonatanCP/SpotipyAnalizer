dbbdc411252aa031323877f0a71cf9c8824918dc
JhonatanCP <jacastillop1108@gmail.com>
2023-05-03 15:40:16 -0500

    se agrego funcion para obtener los track features

--- functions.py
+++ functions.py
@@ -1,40 +1,46 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-def authManager(cliendId,secretId):
+
+def authManager(cliendId, secretId):
     auth_manager = SpotifyClientCredentials(client_id=cliendId, client_secret=secretId)
     return spotipy.Spotify(auth_manager=auth_manager)
 
-def serchPlaylistByName(spotifyAuth,playlistName):
-    playlists = spotifyAuth.search(q=playlistName, type='playlist')['playlists']['items']
-    playlist_id = playlists[0]['id']
+
+def serchPlaylistByName(spotifyAuth, playlistName):
+    playlists = spotifyAuth.search(q=playlistName, type="playlist")["playlists"][
+        "items"
+    ]
+    playlist_id = playlists[0]["id"]
     return spotifyAuth.playlist_tracks(playlist_id, offset=0)
 
-def getAllItems(spotifyAuth,playList):
+
+def getAllItems(spotifyAuth, playList):
     playlist_tracks = []
     cont = 0
-    playlist_tracks += playList['items']
+    playlist_tracks += playList["items"]
 
-    print("Obteniendo Items....\n")
-    while playList['next']:
-        cont +=1
+    print("\n..Obteniendo Items..\n")
+    while playList["next"]:
+        cont += 1
         playList = spotifyAuth.next(playList)
-        playlist_tracks += playList['items']
-        print(f"..Page {cont}..\n")
-    print("Playlist completa")
+        playlist_tracks += playList["items"]
+        print(f"..Page {cont}..")
+    print("Playlist completa\n")
 
     return playlist_tracks
 
+
 def getTracksData(playlist_tracks):
-    tracks= []
+    tracks = []
 
     for track in playlist_tracks:
-        albumName = track['track']['album']['name']
-        trackName = track['track']['name']
-        artistsName = track['track']['artists'][0]['name']
-        durationMs = track['track']['duration_ms']
-        popularity = track['track']['popularity']
-        # audio_features = sp.audio_features(track['track']['id'])[0]
+        idTrack = track["track"]["id"]
+        albumName = track["track"]["album"]["name"]
+        trackName = track["track"]["name"]
+        artistsName = track["track"]["artists"][0]["name"]
+        durationMs = track["track"]["duration_ms"]
+        popularity = track["track"]["popularity"]
 
         # audio_features.pop('type')
         # audio_features.pop('id')
@@ -43,6 +49,26 @@ def getTracksData(playlist_tracks):
         # audio_features.pop('analysis_url')
         # audio_features.pop('duration_ms')
 
-        tracks.append({'Album Name':albumName,'Track Name': trackName, 'Artist Name': artistsName, 'Duration in ms': durationMs,'Popularity': popularity} )
-    
+        tracks.append(
+            {
+                "Id Track": idTrack,
+                "Album Name": albumName,
+                "Track Name": trackName,
+                "Artist Name": artistsName,
+                "Duration in ms": durationMs,
+                "Popularity": popularity,
+            }
+        )
+
     return tracks
+
+
+def getAudioFeatures(spotifyAuth, ids):
+    audioFeatures = []
+    print("\n..Obteniendo Features..\n")
+    while ids:
+        audioFeatures += spotifyAuth.audio_features(ids[:100])
+        print(f"faltan {len(ids)} items por evaluar ...")
+        ids = ids[100:]
+    print("Consulta completa\n")
+    return audioFeatures
--- spotianalis.py
+++ spotianalis.py
@@ -1,41 +1,18 @@
-import spotipy
 import functions as fn
 import pandas as pd
 import numpy as np
 
-from spotipy.oauth2 import SpotifyClientCredentials
 
+client_id = "c0106e614a6e4d2ebca46fd5401e7be4"
+client_secret = "dfb41a1a744f416db305ca6185074c2d"
 
-client_id = 'c0106e614a6e4d2ebca46fd5401e7be4'
-client_secret = 'dfb41a1a744f416db305ca6185074c2d'
-
-sp = fn.authManager(client_id,client_secret)
-playList = fn.serchPlaylistByName(sp,'AllMyMind')
-playlist_tracks = fn.getAllItems(sp,playList)
+sp = fn.authManager(client_id, client_secret)
+playList = fn.serchPlaylistByName(sp, "AllMyMind")
+playlist_tracks = fn.getAllItems(sp, playList)
 tracks = fn.getTracksData(playlist_tracks)
 
-# tracks = []
+tracksData = pd.DataFrame(tracks)
+print(tracksData)
 
-# for track in playlist_tracks:
-
-#     albumName = track['track']['album']['name']
-#     trackName = track['track']['name']
-#     artistsName = track['track']['artists'][0]['name']
-#     durationMs = track['track']['duration_ms']
-#     popularity = track['track']['popularity']
-#     # audio_features = sp.audio_features(track['track']['id'])[0]
-
-#     # audio_features.pop('type')
-#     # audio_features.pop('id')
-#     # audio_features.pop('uri')
-#     # audio_features.pop('track_href')
-#     # audio_features.pop('analysis_url')
-#     # audio_features.pop('duration_ms')
-
-#     tracks.append({'Album Name':albumName,'Track Name': trackName, 'Artist Name': artistsName, 'Duration in ms': durationMs,'Popularity': popularity} )
-
-df2 = pd.DataFrame(tracks)
-print(df2)
-
-
-
+features = fn.getAudioFeatures(sp, tracksData["Id Track"].tolist())
+print(len(features))
