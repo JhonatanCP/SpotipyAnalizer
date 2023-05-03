879195f7cb8a8553b144ad1173739440ec18ce93
JhonatanCP <jacastillop1108@gmail.com>
2023-05-03 12:55:36 -0500

    se separo la funcion gettracksData

--- functions.py
+++ functions.py
@@ -24,3 +24,25 @@ def getAllItems(spotifyAuth,playList):
     print("Playlist completa")
 
     return playlist_tracks
+
+def getTracksData(playlist_tracks):
+    tracks= []
+
+    for track in playlist_tracks:
+        albumName = track['track']['album']['name']
+        trackName = track['track']['name']
+        artistsName = track['track']['artists'][0]['name']
+        durationMs = track['track']['duration_ms']
+        popularity = track['track']['popularity']
+        # audio_features = sp.audio_features(track['track']['id'])[0]
+
+        # audio_features.pop('type')
+        # audio_features.pop('id')
+        # audio_features.pop('uri')
+        # audio_features.pop('track_href')
+        # audio_features.pop('analysis_url')
+        # audio_features.pop('duration_ms')
+
+        tracks.append({'Album Name':albumName,'Track Name': trackName, 'Artist Name': artistsName, 'Duration in ms': durationMs,'Popularity': popularity} )
+    
+    return tracks
--- spotianalis.py
+++ spotianalis.py
@@ -12,26 +12,27 @@ client_secret = 'dfb41a1a744f416db305ca6185074c2d'
 sp = fn.authManager(client_id,client_secret)
 playList = fn.serchPlaylistByName(sp,'AllMyMind')
 playlist_tracks = fn.getAllItems(sp,playList)
+tracks = fn.getTracksData(playlist_tracks)
 
-tracks= []
+# tracks = []
 
-for track in playlist_tracks:
+# for track in playlist_tracks:
 
-    albumName = track['track']['album']['name']
-    trackName = track['track']['name']
-    artistsName = track['track']['artists'][0]['name']
-    durationMs = track['track']['duration_ms']
-    popularity = track['track']['popularity']
-    # audio_features = sp.audio_features(track['track']['id'])[0]
+#     albumName = track['track']['album']['name']
+#     trackName = track['track']['name']
+#     artistsName = track['track']['artists'][0]['name']
+#     durationMs = track['track']['duration_ms']
+#     popularity = track['track']['popularity']
+#     # audio_features = sp.audio_features(track['track']['id'])[0]
 
-    # audio_features.pop('type')
-    # audio_features.pop('id')
-    # audio_features.pop('uri')
-    # audio_features.pop('track_href')
-    # audio_features.pop('analysis_url')
-    # audio_features.pop('duration_ms')
+#     # audio_features.pop('type')
+#     # audio_features.pop('id')
+#     # audio_features.pop('uri')
+#     # audio_features.pop('track_href')
+#     # audio_features.pop('analysis_url')
+#     # audio_features.pop('duration_ms')
 
-    tracks.append({'Album Name':albumName,'Track Name': trackName, 'Artist Name': artistsName, 'Duration in ms': durationMs,'Popularity': popularity} )
+#     tracks.append({'Album Name':albumName,'Track Name': trackName, 'Artist Name': artistsName, 'Duration in ms': durationMs,'Popularity': popularity} )
 
 df2 = pd.DataFrame(tracks)
 print(df2)
